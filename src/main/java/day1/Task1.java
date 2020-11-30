b3496b4fe61cf5fefaced76561072f8cb103af60
Kirr87 <bespokojnyj@gmail.com>
2020-11-30 09:59:10 +0100

    complete day1 tasks

--- src/main/java/day1/Task1.java
+++ src/main/java/day1/Task1.java
@@ -2,6 +2,15 @@ package day1;
 
 public class Task1 {
     public static void main(String[] args) {
+        int count = 0;
+        while (true){
+
+            if (count < 10) {
+                System.out.print("JAVA ");
+                count++;
+            }
+            else break;
+        }
 
     }
 }
--- src/main/java/day1/Task6.java
+++ src/main/java/day1/Task6.java
@@ -2,6 +2,11 @@ package day1;
 
 public class Task6 {
     public static void main(String[] args) {
+        int k = 5;
 
+        for (int i = 1; i < 10; i++){
+            System.out.printf("%d x %d = %d\n", i, k, i * k);
+        }
     }
+
 }
