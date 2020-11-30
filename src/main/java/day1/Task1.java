7aa43d51a68e5a85abc392f61db0eae3cf4d270c
Kirr87 <bespokojnyj@gmail.com>
2020-11-30 22:50:07 +0100

    regfactor Day1 Task1

--- src/main/java/day1/Task1.java
+++ src/main/java/day1/Task1.java
@@ -3,13 +3,9 @@ package day1;
 public class Task1 {
     public static void main(String[] args) {
         int count = 0;
-        while (true){
-
-            if (count < 10) {
+        while (count < 10){
                 System.out.print("JAVA ");
                 count++;
-            }
-            else break;
         }
 
     }
