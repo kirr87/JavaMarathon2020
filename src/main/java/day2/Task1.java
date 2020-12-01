f590ae37df0ee2e946f3725d0f09be547914a180
Kirr87 <bespokojnyj@gmail.com>
2020-12-01 10:31:06 +0100

    complete day2 tasks

--- src/main/java/day2/Task1.java
+++ src/main/java/day2/Task1.java
@@ -1,7 +1,22 @@
 package day2;
 
+import java.util.Scanner;
+
 public class Task1 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        int houseFloor = scanner.nextInt();
+
+        if (houseFloor >= 1 && houseFloor < 5){
+            System.out.println("Малоэтажный дом");
+        }
+        else if (houseFloor > 4 && houseFloor < 9){
+            System.out.println("Среднеэтажный дом");
+        }
+        else if (houseFloor >= 9){
+            System.out.println("Многоэтажный дом");
+        }
+        else System.out.println("Ошибка ввода");
     }
 }
