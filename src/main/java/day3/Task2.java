30ed2ee0f68839280f32fae21048e535b180840c
Kirr87 <bespokojnyj@gmail.com>
2020-12-02 11:17:51 +0100

    complete day3 tasks

--- src/main/java/day3/Task2.java
+++ src/main/java/day3/Task2.java
@@ -1,7 +1,22 @@
 package day3;
 
+import java.util.Scanner;
+
 public class Task2 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        while (true) {
+            double dividend = scanner.nextDouble();
+            double divider = scanner.nextDouble();
+
+            if (divider == 0){
+                break;
+            }
+            else {
+                double result = dividend / divider;
+                System.out.println(result);
+            }
+        }
     }
 }
