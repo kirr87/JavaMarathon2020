d6a1a45a670d0574c12c2b8b4254408ea5f7f09c
Kirr87 <bespokojnyj@gmail.com>
2020-12-06 14:58:59 +0100

    refactor Day4 Task4

--- src/main/java/day4/Task4.java
+++ src/main/java/day4/Task4.java
@@ -13,7 +13,7 @@ public class Task4 {
             array[i] = (int) (Math.random() * 10000);
         }
 
-        for (int i = 1; i < array.length - 1; i++) {
+        /*for (int i = 1; i < array.length - 1; i++) {
             lowArray = Arrays.copyOfRange(array, i - 1, i + 2);
             for (int j = 0; j < lowArray.length; j++) {
                 sumTripleOne += lowArray[j];
@@ -23,8 +23,23 @@ public class Task4 {
                 index = i - 1;
             }
             sumTripleOne = 0;
-        }
+        }*/
 
+        //Вариант решения без дополнительного массива
+
+        for (int i = 1; i < array.length - 3; i++) {
+            //lowArray = Arrays.copyOfRange(array, i - 1, i + 2);
+            for (int j = i; j < i + 3; j++) {
+                sumTripleOne += array[j];
+            }
+            if (sumTripleOne > sumTripleTwo) {
+                sumTripleTwo = sumTripleOne;
+                index = i;
+            }
+            sumTripleOne = 0;
+
+
+        }
         System.out.println(sumTripleTwo);
         System.out.println(index);
     }
